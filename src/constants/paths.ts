5f2149530bc62b1d1c7227605195afe1b98614e9
Mohame ALnawjha <m.nawjha@gmail.com>
2026-03-11 12:09:33 +0200

    feat: add navigation paths constants

---
+++ src/constants/paths.ts
@@ -0,0 +1,54 @@
+
+export const PATHS = {
+    home: '/',
+    services: '#',
+    kooperation: '#',
+    buchfuhrung: '/buchfuehrung',
+    firmenberatung: '/firmenberatung',
+    lohneUndGehalter: '/loehne-und-gehaelter',
+    uberUns: '/ueber-uns',
+    kontakt: '/kontakt',
+    umsatzsteuer: '/umsatzsteuer',
+    einkommensteuer: '/einkommensteuer',
+    gewerbesteuer: '/gewerbesteuer',
+    korperschaftssteuer: '/koerperschaftssteuer',
+};
+
+export const NAVIGATE_LINKS = [
+    {
+        id: 1,
+        label: 'Home',
+        path: PATHS.home
+    },
+    {
+        id: 2,
+        label: 'Services',
+        path: PATHS.services,
+        children: [
+            {
+                id: 21,
+                label: 'Kooperation',
+                path: PATHS.kooperation,
+                children: [
+                    { id: 211, label: 'Umsatzsteuer', path: PATHS.umsatzsteuer },
+                    { id: 212, label: 'Einkommenssteuer', path: PATHS.einkommensteuer },
+                    { id: 213, label: 'Gewerbesteuer', path: PATHS.gewerbesteuer },
+                    { id: 214, label: 'Körperschaftssteuer', path: PATHS.korperschaftssteuer },
+                ]
+            },
+            { id: 22, label: 'Buchführung', path: PATHS.buchfuhrung },
+            { id: 23, label: 'Firmenberatung', path: PATHS.firmenberatung },
+            { id: 24, label: 'Löhne und Gehälter', path: PATHS.lohneUndGehalter },
+        ]
+    },
+    {
+        id: 3,
+        label: 'Über uns',
+        path: PATHS.uberUns
+    },
+    {
+        id: 4,
+        label: 'Kontakt',
+        path: PATHS.kontakt
+    }
+];
