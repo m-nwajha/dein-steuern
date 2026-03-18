fc56a510c0a8c5ace0bbe19999c2c8e1c5a6ccab
Mohame ALnawjha <m.nawjha@gmail.com>
2026-03-18 12:04:50 +0200

    feat: add backgroundImage prop with default fallback to HeroService

--- src/components/organisms/HeroService.tsx
+++ src/components/organisms/HeroService.tsx
@@ -1,16 +1,29 @@
-'use client';
-
+import Image from 'next/image';
 import { Container } from '../ui';
 import BackButton from '../atoms/BackButton';
 
-const HeroService = () => {
+const DEFAULT_IMAGE = '/assets/images/d3697dc9a10cecfb5e0c932cb3f424fb5b164826.jpg';
+
+type HeroServiceProps = {
+    backgroundImage?: string;
+};
+
+const HeroService = ({ backgroundImage = DEFAULT_IMAGE }: HeroServiceProps) => {
     return (
         <section
             id='hero'
-            className="min-h-[calc(100vh-110px)] lg:h-[calc(115vh-110px)] overflow-hidden bg-[url('/assets/images/d3697dc9a10cecfb5e0c932cb3f424fb5b164826.jpg')] bg-cover bg-center bg-no-repeat"
+            className="relative min-h-[calc(100vh-110px)] lg:h-[calc(115vh-110px)] overflow-hidden"
         >
-            <Container variant='xl' className='mt-[130px]'>
-               <BackButton />
+            <Image
+                src={backgroundImage}
+                alt="Service Hero Background"
+                fill
+                priority
+                style={{ objectFit: 'cover', objectPosition: 'center' }}
+                className="-z-10"
+            />
+            <Container variant='xl' className='mt-[130px] relative z-10'>
+                <BackButton />
             </Container>
         </section>
     );
