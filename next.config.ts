15dee3eaa48f9b4938a22105143489a10c9875a3
m-nwajha <m.nawjha@gmail.com>
2026-04-20 12:27:53 +0300

    edit next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'header', key: 'x-forwarded-proto', value: 'http' }],
+        destination: 'https://www.deinesteuern.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'deinesteuern.com' }],
+        destination: 'https://www.deinesteuern.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
